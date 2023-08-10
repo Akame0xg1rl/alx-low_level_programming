5180fbfb9d6df0ce3520f37978c0f3a29f546cc2
Akame0xg1rl <ikrambouachra2@gmail.com>
2023-08-10 16:08:34 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
-int main() 
-{
-    puts("\"Programming is like building a multilingual puzzle");
+
+/**
+ * main - prints "Programming is like building a multilingual
+ *  puzzle, followed by a newline."
+ *  return 0.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    puts("\"Programming is like building a multilingual puzzle\"");
     return 0;
 }
+
 
