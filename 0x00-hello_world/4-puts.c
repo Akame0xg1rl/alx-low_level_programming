8c11e7395c13a44347aa2ed4d5ecb3708840124e
Akame0xg1rl <ikrambouachra2@gmail.com>
2023-08-10 16:18:28 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
-
-
 /**
  * main - main function
- *  return : 0
+ *  Return: 0
  */
-
-#include <stdio.h>
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
