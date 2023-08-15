9bd07618640ca372090e705b62e91a66a6ee0f65
Akame0xg1rl <ikrambouachra2@gmail.com>
2023-08-15 08:48:22 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h> // Include standard library header for the write function
+
+int _putchar(char c); // Prototype for the _putchar function
+
+#endif /* MAIN_H */
