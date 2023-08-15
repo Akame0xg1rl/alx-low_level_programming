eb9409f483219d99ebffea931332d6bcb49319ed
Akame0xg1rl <ikrambouachra2@gmail.com>
2023-08-15 08:40:34 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * Prints the message "_putchar" without using standard library.
+ * Return: 0
+ */
+int main(void)
+{
+    char message[] = "_putchar\n";
+    write(1, message, sizeof(message) - 1);
+
+    return (0);
+}
