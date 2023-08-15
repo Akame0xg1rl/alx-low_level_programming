26a8d02ab6932e28262ef5958f5b39a7f7401203
Akame0xg1rl <ikrambouachra2@gmail.com>
2023-08-15 10:40:07 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10, prints 10 times the alphabet, in lowercase, 
  * Return: Always 0 (Success).
@@ -6,8 +7,10 @@
 void print_alphabet_x10(void)
 {
 	char let;
-	int  l; 
+	int  l;
+
 	l = 0;
+
 	while (l < 10)
 	{
 		let = 'a';
