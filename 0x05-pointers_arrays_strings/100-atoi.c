adce5ef62bdcc9182a9af561a13e0492b3f02de2
Akame0xg1rl <ikrambouachra2@gmail.com>
2023-08-21 19:17:40 -0700

    advanced

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,38 +2,37 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
- *
  * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int index = 0;   
-	int dashCount = 0; 
-	int number = 0;   
-	int length = 0;   
-	int foundNumber = 0; 
-	int currentDigit = 0;
+int index = 0;   
+int dashCount = 0; 
+int number = 0;   
+int length = 0;   
+int foundNumber = 0; 
+int currentDigit = 0;
 
-	while (s[length] != '\0')
-		length++;
-	while (index < length && foundNumber == 0)
+while (s[length] != '\0')
+	length++;
+while (index < length && foundNumber == 0)
 	{
-		if (s[index] == '-')
-			++dashCount;
-		if (s[index] >= '0' && s[index] <= '9')
-		{
-			currentDigit = s[index] - '0';
-			if (dashCount % 2)
-				currentDigit = -currentDigit;
-			number = number * 10 + currentDigit;
-			foundNumber = 1;
-			if (s[index + 1] < '0' || s[index + 1] > '9')
-				break;
-			foundNumber = 0;
-		}
-		index++;
+if (s[index] == '-')
+	++dashCount;
+if (s[index] >= '0' && s[index] <= '9')
+	{
+	currentDigit = s[index] - '0';
+if (dashCount % 2)
+	currentDigit = -currentDigit;
+        number = number * 10 + currentDigit;
+        foundNumber = 1;
+if (s[index + 1] < '0' || s[index + 1] > '9')
+        break;
+	foundNumber = 0;
 	}
-	if (foundNumber == 0)
-		return (0);
+	index++;
+	}
+if (foundNumber == 0)
+        return (0);
 	return (number);
 }
